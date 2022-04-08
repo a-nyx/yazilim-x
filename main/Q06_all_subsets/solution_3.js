a881d22ea81a8bde02d5d7b9181f0ed435777378
Adam Nyx <NyxAdam@protonmail.com>
2022-04-08 18:18:57 -0400

    simplify #06.3

--- main/Q06_all_subsets/solution_3.js
+++ main/Q06_all_subsets/solution_3.js
@@ -3,19 +3,18 @@ const examples = require('./examples');
 const all_subsets = nums => {
     let subsets = [];
 
-    const build_subset = (length, start_index, current_subset) => {
-        if (length < 0) return;
+    const build_subset = (start_index, current_subset) => {
+
+        subsets.push([...current_subset]);
 
         for (let index = start_index; index < nums.length; index++) {
             current_subset.push(nums[index]);
-            build_subset(length - 1, index + 1, current_subset);
+            build_subset(index + 1, current_subset);
             current_subset.pop();
         }
-
-        subsets.push([...current_subset]);
     };
 
-    build_subset(nums.length, 0, []);
+    build_subset(0, []);
     return subsets;
 };
 
