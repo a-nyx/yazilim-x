0e53765ae256b7c178ff9a7a67996a75615fbe8d
Adam Nyx <NyxAdam@protonmail.com>
2022-04-11 23:05:07 -0400

    add #08.1 and #08.2

--- main/Q08_three_sum/solution_1.php
+++ main/Q08_three_sum/solution_1.php
@@ -2,14 +2,45 @@
 
 include 'examples.php';
 
-function three_sum($nums, $target)
+function three_sum($nums)
 {
-    return null;
+    // find all triplets
+    $answers = [];
+    for ($x = 0; $x < count($nums); $x++) {
+        for ($y = $x + 1; $y < count($nums); $y++) {
+            for ($z = $y + 1; $z < count($nums); $z++) {
+                if ($nums[$x] + $nums[$y] + $nums[$z] == 0) {
+                    $answer = [$nums[$x], $nums[$y], $nums[$z]];
+                    sort($answer);
+                    $answers[] = $answer;
+                }
+            }
+        }
+    }
+
+    // remove duplicates
+    for ($i = 0; $i < count($answers); $i++) {
+        for ($k = $i + 1; $k < count($answers); $k++) {
+            if ($answers[$i] == $answers[$k]) {
+                array_splice($answers, $k, 1);
+                $k--;
+            }
+        }
+    }
+
+    return $answers;
 }
 
 
-foreach ($examples as $target => $nums) {
-    echo "input: [" . implode(',', $nums) . "] & " . $target . PHP_EOL
-        . "answer: [" . implode(',', three_sum($nums, $target)) . "]" . PHP_EOL
-        . '---------------' . PHP_EOL;
+foreach ($examples as $example) {
+    $results = three_sum($example);
+    $answers = "";
+    foreach ($results as $answer) {
+        $answers .= PHP_EOL . implode(",", $answer);
+    }
+    $output = "input: [" . implode(',', $example) . "]" . PHP_EOL
+        . "answers: " . $answers . PHP_EOL
+        . '---------------';
+
+    echo $output;
 }
--- main/Q08_three_sum/solution_2.php
+++ main/Q08_three_sum/solution_2.php
@@ -2,14 +2,49 @@
 
 include 'examples.php';
 
-function three_sum($nums, $target)
+function three_sum($nums)
 {
-    return null;
+    sort($nums);
+    $answers = [];
+
+    for($i = 0; $i < count($nums)-2; $i++) {
+        if($i > 0 && $nums[$i] == $nums[$i-1]) {
+            continue;
+        }
+
+        $subtotal = 0 - $nums[$i];
+        $left = $i + 1;
+        $right = count($nums) - 1;
+
+        while($left < $right) {
+            if($nums[$left] + $nums[$right] == $subtotal) {
+                $answers[] = [$nums[$i], $nums[$left], $nums[$right]];
+                $left++;
+                $right--;
+                while($left < $right && $nums[$left] == $nums[$left-1]) {
+                    $left++;
+                }
+            } else if($nums[$left] + $nums[$right] < $subtotal) {
+                $left++;
+            } else {
+                $right--;
+            }
+        }
+    }
+
+    return $answers;
 }
 
 
-foreach ($examples as $target => $nums) {
-    echo "input: [" . implode(',', $nums) . "] & " . $target . PHP_EOL
-        . "answer: [" . implode(',', three_sum($nums, $target)) . "]" . PHP_EOL
-        . '---------------' . PHP_EOL;
+foreach ($examples as $example) {
+    $results = three_sum($example);
+    $answers = "";
+    foreach ($results as $answer) {
+        $answers .= PHP_EOL . implode(",", $answer);
+    }
+    $output = "input: [" . implode(',', $example) . "]" . PHP_EOL
+        . "answers: " . $answers . PHP_EOL
+        . '---------------';
+
+    echo $output;
 }
