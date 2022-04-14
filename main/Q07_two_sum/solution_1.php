2e955eb5407baa0ce8dd7b68c0f04bcc6c4db647
Adam Nyx <NyxAdam@protonmail.com>
2022-04-13 22:39:54 -0400

    fix #07

--- main/Q07_two_sum/solution_1.php
+++ main/Q07_two_sum/solution_1.php
@@ -4,14 +4,13 @@ include 'examples.php';
 
 function two_sum($nums, $target)
 {
-    for ($first = 0; $first < count($nums); $first++) {
-        for ($second = $first + 1; $second < count($nums); $second++) {
-            if ($nums[$first] + $nums[$second] == $target) {
-                $answer = [$first, $second];
+    for($first = 0; $first < count($nums); $first++){
+        for($second = $first + 1; $second < count($nums); $second++){
+            if($nums[$first] + $nums[$second] == $target){
+                return [$first, $second];
             }
         }
     }
-    return $answer ?? null;
 }
 
 
