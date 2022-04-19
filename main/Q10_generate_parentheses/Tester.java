4b9f951aa184f19247ca69be808bdc3da7dd9de2
Adam Nyx <NyxAdam@protonmail.com>
2022-04-18 20:06:54 -0400

    q10 tester

--- main/Q10_generate_parentheses/Tester.java
+++ main/Q10_generate_parentheses/Tester.java
@@ -3,7 +3,11 @@ import java.util.*;
 
 public class Tester {
     public static void main(String[] args) {
-        List<String> list = Solution1.generateParentheses(2);
-        System.out.println(Arrays.toString(list.toArray()));
+        for (int i = 1; i < 5; i++) {
+            List<String> list = Solution1.generateParentheses(i);
+            System.out.println("---------------");
+            System.out.println("n: " + i);
+            System.out.println(String.join("\n", list));
+        }
     }
 }
