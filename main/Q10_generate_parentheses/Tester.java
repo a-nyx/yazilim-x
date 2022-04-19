b3eb8c6ccf5436c6a24869a538b090f338fd1fea
Adam Nyx <NyxAdam@protonmail.com>
2022-04-18 20:58:19 -0400

    q10 s2

--- main/Q10_generate_parentheses/Tester.java
+++ main/Q10_generate_parentheses/Tester.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Tester {
     public static void main(String[] args) {
         for (int i = 1; i < 5; i++) {
-            List<String> list = Solution1B.generateParentheses(i);
+            List<String> list = Solution2.generateParentheses(i);
             System.out.println("---------------");
             System.out.println("n: " + i + " => size: " + list.size());
             System.out.println(String.join("\n", list));
