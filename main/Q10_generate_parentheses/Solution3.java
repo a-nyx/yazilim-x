418732afd18bee51b27ae9954d9a414235a09369
Adam Nyx <NyxAdam@protonmail.com>
2022-04-18 22:26:48 -0400

    add dp for #10

--- main/Q10_generate_parentheses/Solution3.java
+++ main/Q10_generate_parentheses/Solution3.java
@@ -20,4 +20,24 @@ public class Solution3 {
 
         return list;
     }
+
+    public static List<String> generateParentheses(int n) {
+        Map<Integer,List<String>> solutions = new HashMap<Integer,List<String>>();
+        solutions.put(0, Arrays.asList(""));
+
+        for(int k = 1; k <= n; k++){
+            List<String> list = new ArrayList<String>();
+            for(int i = 0; i < k; i++){
+                for(String left : solutions.get(i)){
+                    for(String inside : solutions.get(k-i-1)){
+                        list.add(left + "(" + inside + ")");
+                    }
+                }
+            }
+
+            solutions.put(k, list);
+        }
+
+        return solutions.get(n);
+    }
 }
