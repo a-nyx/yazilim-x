aab90aaf86ad0dd21c9dd9e13ee931246049669e
Adam Nyx <NyxAdam@protonmail.com>
2022-04-18 20:42:34 -0400

    10.1b

--- main/Q10_generate_parentheses/Solution1B.java
+++ main/Q10_generate_parentheses/Solution1B.java
@@ -5,53 +5,40 @@ import java.util.*;
 public class Solution1B {
     public static List<String> generateParentheses(int n) {
         List<String> list = new ArrayList<String>();
-        generateAll(list, n);
-        deleteInvalid(list);
+        generateAll(list, new char[2*n], 0);
         return list;
     }
 
-    public static void generateAll(List<String> list, int n) {
-        int amount = (int) Math.pow(2, 2 * n);
-        for (int i = 0; i < amount; i++) {
-            list.add(binaryToString(i, n));
+    public static void generateAll(List<String> list, char[] chars, int index) {
+        if (index == chars.length) {
+            String s = new String(chars);
+            if (isValid(s)) {
+                list.add(new String(chars));
+            }
+            return;
         }
+
+        chars[index] = '(';
+        generateAll(list, chars, index+1);
+        chars[index] = ')';
+        generateAll(list, chars, index + 1);
     }
 
-    public static String binaryToString(int binary, int n) {
-        char[] chars = new char[2 * n];
-        for (int i = 0; i < 2 * n; i++) {
-            if ((binary & 1) == 1) {
-                chars[i] = '(';
+    public static boolean isValid(String str) {
+        int open = 0;
+        for (int k = 0; k < str.length(); k++) {
+            if (str.charAt(k) == '(') {
+                open++;
             } else {
-                chars[i] = ')';
+                open--;
             }
-            binary = binary >> 1;
-        }
-        return new String(chars);
-    }
-
-    public static void deleteInvalid(List<String> list) {
-        for (int i = 0; i < list.size(); i++) {
-            int open = 0;
-            boolean shouldDelete = false;
-            for (int k = 0; k < list.get(i).length(); k++) {
-                if (list.get(i).charAt(k) == '(') {
-                    open++;
-                } else {
-                    open--;
-                }
-                if (open < 0) {
-                    shouldDelete = true;
-                    break;
-                }
-            }
-            if (open != 0) {
-                shouldDelete = true;
-            }
-            if (shouldDelete) {
-                list.remove(i);
-                i--;
+            if (open < 0) {
+                return false;
             }
         }
+        if (open != 0) {
+            return false;
+        }
+        return true;
     }
 }
