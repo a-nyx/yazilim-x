94c944dfa44b57889b6c512f2df4c4cd28f2d3c6
Adam Nyx <NyxAdam@protonmail.com>
2022-04-18 21:32:20 -0400

    q10 s3

--- main/Q10_generate_parentheses/Solution3.java
+++ main/Q10_generate_parentheses/Solution3.java
@@ -1,5 +1,23 @@
+package Q10_generate_parentheses;
+
+import java.util.*;
+
 public class Solution3 {
-    public static void main(String[] args) {
-        System.out.println("haha");
+    public static List<String> generateParentheses(int n) {
+        List<String> list = new ArrayList<String>();
+
+        if(n == 0){
+            list.add("");
+        }
+
+        for(int k = 0; k < n; k++){
+            for(String left : generateParentheses(k)){
+                for(String inside : generateParentheses(n-k-1)){
+                    list.add(left + "(" + inside + ")");
+                }
+            }
+        }
+
+        return list;
     }
 }
