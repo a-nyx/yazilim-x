8a93e53fcf699758cdc2126c3e37dff4c3458263
Adam Nyx <NyxAdam@protonmail.com>
2022-05-23 22:20:47 -0400

    init Q14

---
+++ main/Q14_palindrome_permutation/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+namespace Q14_palindrome_permutation
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("yo!");
+    }
+  }
+}
