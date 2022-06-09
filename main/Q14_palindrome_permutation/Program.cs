10d66ac95190f2a989fafc8dcff79177c849b611
Adam Nyx <NyxAdam@protonmail.com>
2022-06-09 14:47:01 -0400

    solve #14

--- main/Q14_palindrome_permutation/Program.cs
+++ main/Q14_palindrome_permutation/Program.cs
@@ -1,13 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-namespace Q14_palindrome_permutation
+﻿class Program
 {
-  class Program
+  public static void Main(string[] args)
   {
-    public static void Main(string[] args)
+    Dictionary<string, bool> tests = new Dictionary<string, bool>{
+        {"hahahaha", true},
+        {"hahahahaha", false},
+        {"asdfgasdf", true},
+        {"akdhfgdfdhjkfaggdafjhad", false},
+        {"   ", true},
+        {"  a ", true},
+        {"aaaaaaabbbbbb", true},
+        {"ey edip adanada pide ye", true},
+        {"xy", false},
+        {"abcdefghijklmnopqrstuvwxyz", false}
+      };
+
+    bool isSuccessful = true;
+    foreach (KeyValuePair<string, bool> entry in tests)
     {
-      Console.WriteLine("yo!");
+      bool actual_result = Solution2.IsPalindromePermutation2(entry.Key);
+      String test_result = actual_result == entry.Value ? "PASSED" : "FAILED";
+
+      if(actual_result != entry.Value)
+      {
+        isSuccessful = false;
+      }
+
+      Console.WriteLine($@"{test_result}:
+          Expected: {entry.Value.ToString()}
+          Solution: {actual_result.ToString()}
+          Input: {entry.Key}");
+    }
+
+    if(isSuccessful)
+    {
+      Console.WriteLine("All tests passed. Yay!");
     }
   }
 }
