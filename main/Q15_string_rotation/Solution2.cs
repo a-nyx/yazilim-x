086d0d5f50fbadb7fff20c78567d6d881df41409
Adam Nyx <NyxAdam@protonmail.com>
2022-06-11 13:00:56 -0400

    fix 15.2

--- main/Q15_string_rotation/Solution2.cs
+++ main/Q15_string_rotation/Solution2.cs
@@ -2,6 +2,8 @@ public class Solution2
 {
   public static bool RotateString(string s1, string s2)
   {
+    if (s1.Length != s2.Length) return false;
+    if (s1 == "") return true;
     return (s1 + s1).Contains(s2);
   }
 
