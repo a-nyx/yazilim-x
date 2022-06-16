da0091307b494add83f1f815f3c6baf5a03666b0
Adam Nyx <NyxAdam@protonmail.com>
2022-06-16 19:51:47 -0400

    init 15.3

--- main/Q15_string_rotation/Solution3.cs
+++ main/Q15_string_rotation/Solution3.cs
@@ -1,7 +1,47 @@
 public class Solution3
 {
-  public static bool RotateString(string s1, string s2)
+  const int MOD = 1_000_000_007;
+  const int PRIME = 113;
+  const int PRIME_INVERSE = 893805316;
+  int power = 1;
+
+  public bool RotateString(string s1, string s2)
   {
+    if (s1.Length != s2.Length) return false;
+    if (s1 == "") return true;
+
+    long hashS1 = this.HashString(s1);
+    long hashS2 = this.HashString(s2);
+
+    for (int i = 0; i < s1.Length; i++)
+    {
+      if (hashS1 == hashS2 && s1.Equals(s2)) return true;
+      hashS1 = UpdateHash(s1[i], hashS1);
+    }
+
     return false;
+  }
+
+  public long HashString(string s)
+  {
+    long hash = 0;
+    power = 1;
+
+    foreach (char c in s)
+    {
+      hash = (hash + power * c) % MOD;
+      power = power * PRIME % MOD;
+    }
+
+    return hash;
+  }
+
+  public long UpdateHash(char c, long hash)
+  {
+    hash += power * c - c;
+    hash %= MOD;
+    hash *= PRIME_INVERSE;
+    hash %= MOD;
+    return hash;
   }
 }
