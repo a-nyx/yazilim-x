d1eb9b02d9a10622cb0ae25289be5b6a81c16300
Adam Nyx <NyxAdam@protonmail.com>
2022-07-10 23:47:14 -0400

    add swift directory

---
+++ swift/Q16_queue_using_stacks/main.swift
@@ -0,0 +1,20 @@
+var q = MyQueue1()
+
+q.push(101)
+q.push(202)
+q.push(303)
+q.push(404)
+
+print("Queue: \(q.description)")
+print("Peek: \(q.peek())")
+
+print("Pop: \(q.pop())")
+print("Empty?: \(q.empty())")
+
+print("Pop: \(q.pop())")
+print("Peek: \(q.peek())")
+
+print("Pop: \(q.pop())")
+
+print("Pop: \(q.pop())")
+print("Empty?: \(q.empty())")
